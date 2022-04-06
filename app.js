8ccd998a9da7c03724f2fc165ce2f15a889abf2e
daejah-atkinson <daejah.atkinson@gmail.com>
2022-04-06 12:05:30 -0400

    Very simple pseudocode created for gameplay

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+
+//GAMEPLAY PSEUDOCODE
+
+//1. Game board starts out with two number 2 tiles
+//2. Player uses the arrow keys to slide the tiles on the board
+//3. If two matching tiles are slide together they combine to create a single new tiles with the sum of the two numbers combined
+//4. This continues until player reaches tile 2048 or until the game board is full and no more moves can be made
